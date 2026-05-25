05d72a083bc17fe80c0b862cfd1cfc06d418f607
Martinius <mar109us@gmail.com>
2026-05-25 23:12:28 +0200

    trying to figure out how to catch if no data is sent through socket
    investigating websocket object

--- main.js
+++ main.js
@@ -4,10 +4,16 @@ const output = document.getElementById("output");
 socket.onmessage = (event) => {
 	const data = JSON.parse(event.data);
 
-	if (data.button >= 0) {
+	if (socket.onmessage.length > 0) {
 		output.innerHTML = `${data.button}`;
+		console.log(socket);
+	} 
+	if (socket.onmessage.length === 0) {
+		console.log("none");
 	}
-	if (!data.button) {
+
+	/* 	if (!event) {
 		output.innerHTML = `no button`;
-	}
+		
+	} */
 };
